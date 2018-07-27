14d1823673d2f0cf1b69b7a89367e7a2e2fb2876
Peter.DeRijk <Peter.DeRijk@gmail.com>
2018-07-27 17:01:54 +0000

    instmakekit.sh fixes: dont stop on exe does not exist, locations

--- work/instmakekit.sh
+++ work/instmakekit.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 export opentsvdir=/home/peter/dev/opentsv
 export kitdir=/home/peter/dev/opentsv/kit
 echo "---------- Make opentsv kit ----------"
@@ -5,11 +6,13 @@ cd $kitdir
 cp $opentsvdir/opentsv.tcl $opentsvdir/res/tclkit.inf opentsv.vfs
 echo '' > $kitdir/opentsv.vfs/MANIFEST
 cd $kitdir
-rm opentsv.exe
+rm opentsv.exe || true
 wine tclkit858.exe sdx-20110317.kit wrap opentsv.exe -runtime tclkit-win32.upx.exe
 
-cp -f opentsv.exe ~/move/com
-cp -f opentsv.exe /home/peter/build/tca/Windows-intel/opentsv.exe
+cd ~/dev/opentsv
+cp -f kit/opentsv.exe ~/move/com
+cp -f kit/opentsv.exe /home/peter/build/tca/Windows-intel/opentsv.exe
+cp -f opentsv.tcl ~/move/com
 
 # cp -f /home/peter/dev/opentsv/opentsv.tcl ~/move/tca/Windows-intel/apps/opentsv/
 # cp -f ~/move/tca/Windows-intel/apps/opentsv/opentsv.tcl /home/peter/dev/opentsv
